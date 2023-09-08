428d64cfdf046c8570a9706601413f1614d1bdec
HyunjaeJeon <48283245+HyunJhun@users.noreply.github.com>
2023-09-09 01:21:52 +0900

    Player Move Animation
    일반 상태에서의 움직임과 Lock On 상태에서의 움직임에 관한 애니메이션 전환 기능 구현

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -1,13 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    [Header("Standard Player State")]
+    public enum PlayerState
+    {
+        Idle,
+        Forward,
+        Left,
+        Right,
+        Backward
+    }
+    [Header("PlayerState Property")]
     [SerializeField] private float walkSpeed = 5.0f; // 캐릭터 움직이는 속도
     [SerializeField] private float rotationSpeed = 360f; // 캐릭터가 회전하는 속도
-
+    [SerializeField] private PlayerState baseState;
+    [SerializeField] private bool isLockOn;
     /*
     [Header("Jump Property")]
     [SerializeField] private float gravitationalAcceleration;
@@ -15,14 +22,8 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float gravity;
     private GroundCheck groundChecker;
     */
-    private enum PlayerState
-    {
-        idle = 0,
-        forward = 1,
-        left = 2,
-        right = 3,
-        backward = 4
-    }
+    [Header("Environment Property")]
+    [SerializeField] private float gravity;
 
     Animator playerAnimator;
 
@@ -31,52 +32,100 @@ public class PlayerMove : MonoBehaviour
     void Start()
     {
         player = GetComponent<CharacterController>();
-        playerAnimator = GetComponentInChildren<Animator>();
+        playerAnimator = GetComponent<Animator>();
+        baseState = PlayerState.Idle;
         //groundChecker = GameObject.Find("GroundChecker").GetComponent<GroundCheck>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal") * walkSpeed, 0, Input.GetAxis("Vertical") * walkSpeed);
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-        if(direction.sqrMagnitude > 0.01f)
+        if (direction.sqrMagnitude > 0.01f)
         {
-            Vector3 forward = Vector3.Slerp(
-                transform.forward,
-                direction,
-                rotationSpeed * Time.deltaTime / Vector3.Angle(transform.forward, direction)
-                );
-            transform.LookAt(transform.position + forward);
+            if (isLockOn == false)
+            {//캐릭터가 보고 있는 정면의 방향을 계산해서 보여주는 작업
+                Vector3 forward = Vector3.Slerp(
+                  transform.forward,
+                  direction,
+                  rotationSpeed * Time.deltaTime / Vector3.Angle(transform.forward, direction)
+                  );
+                transform.LookAt(transform.position + forward);
+            }
         }
-        Debug.Log(direction);
-        /*
-        if (Input.GetButtonDown("Jump") && !groundChecker.IsGrounded())
-        {
-            direction.y = jumpForce;
-            Debug.Log("점프키 입력");
-        }
-        direction.y = direction.y - gravity;
-        */
-        if (direction.z > 0) // forward
-        {
-            playerAnimator.SetInteger("State", (int)PlayerState.forward);
-        }
-        else if (direction.z < 0) // backward
-        {
-            playerAnimator.SetInteger("State", (int)PlayerState.backward);
-        }
-        else if (direction.x > 0) // right
-        {
-            playerAnimator.SetInteger("State", (int)PlayerState.right);
-        }
-        else if (direction.x < 0) // left
-        {
-            playerAnimator.SetInteger("State", (int)PlayerState.left);
-        }
-        else
-            playerAnimator.SetInteger("State", (int)PlayerState.idle);
-        player.Move(direction * Time.deltaTime);
+        OnGravity(direction);
+        LockOnCheck();
+        PlayerAnimation(direction);
+        player.Move(direction * walkSpeed * Time.deltaTime);
     }
 
+    private void PlayerAnimation(Vector3 direction)
+    {
+        if (isLockOn == true)
+        {
+            playerAnimator.SetLayerWeight(1, 1);
+            if (direction.x > 0.5)
+            {
+                baseState = PlayerState.Right;
+                playerAnimator.SetInteger("direction", (int)baseState);
+            }
+            else if (direction.x < -0.5)
+            {
+                baseState = PlayerState.Left;
+                playerAnimator.SetInteger("direction", (int)baseState);
+            }
+            else if (direction.z > 0.5)
+            {
+                baseState = PlayerState.Forward;
+                playerAnimator.SetInteger("direction", (int)baseState);
+            }
+            else if (direction.z < -0.5)
+            {
+                baseState = PlayerState.Backward;
+                playerAnimator.SetInteger("direction", (int)baseState);
+            }
+            else
+            {
+                baseState = PlayerState.Idle;
+                playerAnimator.SetInteger("direction", (int)baseState);
+                Debug.Log("IDLE");
+            }
+        }
+        else if (isLockOn == false)
+        {
+            playerAnimator.SetLayerWeight(1, 0);
+            if (direction.magnitude > 0.5) // 플레이어가 움직일 떄
+            {
+                baseState = PlayerState.Forward;
+                playerAnimator.SetInteger("direction", (int)baseState);
+            }
+            else
+            {
+                baseState = PlayerState.Idle;
+                playerAnimator.SetInteger("direction", (int)baseState);
+            }
+        }
+    }
+
+    private void OnGravity(Vector3 direction)
+    {
+        direction.y = direction.y - gravity;
+    }
+
+    private void LockOnCheck()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            isLockOn = true;
+            Debug.Log("MOUSE LOCK");
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            isLockOn = false;
+            Debug.Log("MOUSE DOESNT LOCK");
+        }
+    }
 }
+
+
