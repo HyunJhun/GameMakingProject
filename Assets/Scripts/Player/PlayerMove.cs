1c685a4fb240b8913f01e8c38eb2ac87c5fb04c3
HyunjaeJeon <48283245+HyunJhun@users.noreply.github.com>
2023-09-12 00:11:43 +0900

    Temporary Complete about Player Moving
    Lock On/Off 시 캐릭터의 움직임과 마우스 움직임에 따른 카메라 이동을 수정
    
    문제점 : 1. Lock On 시 Lock이 된 물체에 대해 카메라가 고정이 되므로 카메라가 일정 각도를 벗어나게 되면 카메라에 캐릭터가 보이지 않는 문제 -> 임시로 카메라 각도를 캐릭터가 보이게끔 이동 각도 범위를 제한함 but 최대 각도 도달 후 그 이상으로 움직일 시 최소 각도쪽으로 카메라가 이동하는 현상이 발생
    2. cinemachine을 이용해 카메라를 구현하였으나 완성도가 떨어짐

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -10,9 +10,10 @@ public class PlayerMove : MonoBehaviour
         Right,
         Backward
     }
-    [Header("PlayerState Property")]
+    [Header("Input Property")]
     [SerializeField] private float walkSpeed = 5.0f; // 캐릭터 움직이는 속도
     [SerializeField] private float rotationSpeed = 360f; // 캐릭터가 회전하는 속도
+    //[SerializeField] private float cameraRotationSpeed = 2.0f;
     [SerializeField] private PlayerState baseState;
     [SerializeField] private bool isLockOn;
     /*
@@ -25,10 +26,15 @@ public class PlayerMove : MonoBehaviour
     [Header("Environment Property")]
     [SerializeField] private float gravity;
 
-    Animator playerAnimator;
+    [Header("References")]
+    public ThirdPersonCameraHandler cameraHandler;
 
+    Animator playerAnimator;
     CharacterController player;
     // Start is called before the first frame update
+    private void Awake()
+    {
+    }
     void Start()
     {
         player = GetComponent<CharacterController>();
@@ -41,10 +47,10 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
-        if (direction.sqrMagnitude > 0.01f)
+        Vector3 dirToCombatLookAt = cameraHandler.combatLookAt.position - new Vector3(transform.position.x, cameraHandler.combatLookAt.position.y, transform.position.z);
+        if (direction.sqrMagnitude > 0.01f) // 캐릭터 이동시 움직이는 형태 관련
         {
-            if (isLockOn == false)
+            if (isLockOn == false) // Lock On 시
             {//캐릭터가 보고 있는 정면의 방향을 계산해서 보여주는 작업
                 Vector3 forward = Vector3.Slerp(
                   transform.forward,
@@ -53,13 +59,21 @@ public class PlayerMove : MonoBehaviour
                   );
                 transform.LookAt(transform.position + forward);
             }
+            else // Lock Off 시
+            {
+                Vector3 forward = Vector3.Slerp(
+                    transform.forward,
+                    dirToCombatLookAt,
+                    rotationSpeed * Time.deltaTime / Vector3.Angle(transform.forward, dirToCombatLookAt)
+                    );
+                transform.LookAt(transform.position + forward);
+            };
         }
         OnGravity(direction);
-        LockOnCheck();
+        LockOnChanger();
         PlayerAnimation(direction);
         player.Move(direction * walkSpeed * Time.deltaTime);
     }
-
     private void PlayerAnimation(Vector3 direction)
     {
         if (isLockOn == true)
@@ -113,17 +127,22 @@ public class PlayerMove : MonoBehaviour
         direction.y = direction.y - gravity;
     }
 
-    private void LockOnCheck()
+    private void LockOnChanger()
     {
-        if (Input.GetMouseButton(1))
+        if (Input.GetMouseButtonDown(1))
         {
-            isLockOn = true;
-            Debug.Log("MOUSE LOCK");
-        }
-        else if (Input.GetMouseButtonUp(1))
-        {
-            isLockOn = false;
-            Debug.Log("MOUSE DOESNT LOCK");
+            if (isLockOn == false)
+            {
+                isLockOn = true;
+                Debug.Log("MOUSE LOCK");
+                cameraHandler.CurrentStyleChanger();
+            }
+            else
+            {
+                isLockOn = false;
+                Debug.Log("MOUSE DOESNT LOCK");
+                cameraHandler.CurrentStyleChanger();
+            }
         }
     }
 }
---
+++ Assets/Scripts/Player/ThirdPersonCameraHandler.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class ThirdPersonCameraHandler : MonoBehaviour
+{
+    [Header("References")]
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObj;
+    //public PlayerMove playerMoveHandler;
+    public float rotationSpeed;
+    public Transform combatLookAt;
+    private CameraStyle currentCam;
+
+    public GameObject basicCam;
+    public GameObject combatCam;
+    public enum CameraStyle
+    {
+        Basic,
+        LockOn
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        currentCam = CameraStyle.Basic;
+    }
+    
+    private void Update()
+    {
+        // 회전 방향 설정
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
+
+        // 유저 회전 설정
+        if (currentCam == CameraStyle.Basic)
+        {
+            if (basicCam.activeSelf == false && combatCam.activeSelf == true)
+            {
+                basicCam.SetActive(true);
+                combatCam.SetActive(false);
+            }
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            Vector3 inputDir = orientation.forward * v + orientation.right * h;
+
+            if (inputDir != Vector3.zero)
+            {
+                playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+            }
+        }
+        else if (currentCam == CameraStyle.LockOn)
+        {
+            if (combatCam.activeSelf == false && basicCam.activeSelf == true)
+            {
+                combatCam.SetActive(true);
+                basicCam.SetActive(false);
+            }
+            Vector3 dirToCombatLookAt = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
+            orientation.forward = dirToCombatLookAt.normalized;
+
+            playerObj.forward = dirToCombatLookAt.normalized;
+        }
+    }
+
+    public void CurrentStyleChanger()
+    {
+        if (currentCam == CameraStyle.Basic)
+            currentCam = CameraStyle.LockOn;
+        else
+            currentCam = CameraStyle.Basic;
+    }
+
+}
