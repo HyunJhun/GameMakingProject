cc499d60a0d3164bca04ff20372694fe81845e97
HyunjaeJeon <48283245+HyunJhun@users.noreply.github.com>
2023-10-19 18:42:48 +0900

    Chagne ChasingState
    navmesh를 사용하여 구현

--- Assets/Scripts/Enemy/State/BossChase.cs
+++ Assets/Scripts/Enemy/State/BossChase.cs
@@ -13,6 +13,7 @@ public class BossChase : BossState
     // Detect Range Radius
     private static float radius = 7.5f;
     private float chaseRange = radius + 5f;
+    private float attackRange = radius - 3f;
     
 
     private float timer;
@@ -26,10 +27,12 @@ public class BossChase : BossState
             return;
         }
         timer = 0f;
+        boss.agent.stoppingDistance = attackRange;
     }
     public override void Exit()
     {
         Debug.Log("ChaseExit");
+        boss.agent.stoppingDistance = 0f;
     }
     public override void StateActionUpdate()
     {
@@ -39,13 +42,15 @@ public class BossChase : BossState
         // 현재 탐지 범위의 반지름은 7.5 ... distance가 7.5가 최대치여야함
         float distance = Mathf.Abs(Vector3.Distance(boss.gameObject.transform.position, boss.player.transform.position));
         // 보스 회전
-        Vector3 forward = Vector3.Slerp(direction, boss.transform.forward,
-                boss.rotationSpeed * Time.deltaTime / Vector3.Angle(direction, boss.transform.forward));     
+        //Vector3 forward = Vector3.Slerp(direction, boss.transform.forward, boss.rotationSpeed * Time.deltaTime / Vector3.Angle(direction, boss.transform.forward));
+        Debug.Log(chaseRange);
+        Debug.Log(distance);
 
         if (distance <= chaseRange) // 추격 범위 설정
         {
             //boss.transform.LookAt(boss.transform.position - forward);
             //boss.GetComponent<Rigidbody>().transform.Translate(direction * boss.moveSpeed * Time.deltaTime);
+            Debug.Log("추격 범위 안");
             boss.agent.destination = boss.player.transform.position;
             timer = 0f;
             if (distance < radius - 3f) // 공격 사정 거리
@@ -54,11 +59,13 @@ public class BossChase : BossState
                 return;
             }
         }
-        else if(distance > chaseRange) // 추격 범위를 벗어나면
+        else // 추격 범위를 벗어나면
         {
-            if(timer < 3f) // 플레이어를 놓쳐서 잠시 대기하여 추격 범위에 플레이어가 다시 들어오는지 체크하는 역할
+            boss.agent.destination = boss.transform.position;
+            if (timer < 3f) // 플레이어를 놓쳐서 잠시 대기하여 추격 범위에 플레이어가 다시 들어오는지 체크하는 역할
             {
                 timer += Time.deltaTime;
+                Debug.Log("추격 범위 밖");
                 if (timer % 1 == 0)
                     Debug.Log(timer);
             }
