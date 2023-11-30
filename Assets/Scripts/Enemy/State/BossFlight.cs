5f6396f9748ca573d92891052fe688bd65412e06
HyunjaeJeon <48283245+HyunJhun@users.noreply.github.com>
2023-11-30 12:24:05 +0900

    Others

--- Assets/Scripts/Enemy/State/BossFlight.cs
+++ Assets/Scripts/Enemy/State/BossFlight.cs
@@ -11,21 +11,16 @@ public class BossFlight : BossState
     // property
     private float maxHeightToFly = 2.5f;
     private bool isFly = false;
-    
-
     //reference
     private Rigidbody bossRd;
     public override void Enter()
     {
         phaseTwoInitialize();
-
     }
-
     public override void Exit()
     {
         isFly = false;
     }
-
     public override void StateActionUpdate()
     {
         if(!isFly)
@@ -35,31 +30,22 @@ public class BossFlight : BossState
             bossStateMachine.ChangeState(boss.flyAroundState);
 
     }
-
     public override void StateActionFixedUpdate()
-    {
-        
+    {      
     }
-
     private void phaseTwoInitialize()
     {
         // 페이즈 관련
         boss.isEnterPhaseTwo = true; // 먼저 다시 페이즈 2에 재진입할 일이 없도록 함
-
         // hp 관련
         stats.SetBossHpToMaxHp(); // 먼저 보스의 hp를 다시 전체로 회복시켜 페이즈 2를 시행한다.    
-
         // 움직임 관련
         boss.agent.SetDestination(boss.transform.position); // boss.agent.isStopped 를 통해 움직임을 멈춰버리는 것은 단순히 정지시키는 것이라서 별로임.
         boss.agent.enabled = false; // 잠시 공중에서는 길을 찾을 필요도 없고 또한 navmeshagent가 활성화 되어있다면 항상 땅에 붙어있어 공중이동이 불가하므로 false로 변경
-
-
         // 레퍼런스
-        bossRd = boss.GetComponent<Rigidbody>();
-        
-    }
-    
-    IEnumerator FlyToTheSkyFromGround() // 부자연스럽게 날음.. 임시.(암만봐도 lerp 사용해야 할 것 같은데 그게 쉽지가 않네; 왜 정상 적용이 안되는거야
+        bossRd = boss.GetComponent<Rigidbody>();      
+    }  
+    IEnumerator FlyToTheSkyFromGround()
     {
         yield return null;
         float timer = 0f;
@@ -77,7 +63,5 @@ public class BossFlight : BossState
         }
         yield return null;
     }
-    
-    // y = +2, x.rotation = +28
 }
 
